8bebca29d527a0c4a1d0ea16a978fa25e26e3389
Alisan <144158983+AZCPCF@users.noreply.github.com>
2025-10-15 23:50:34 +0330

    feat: add new porjects

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -1,7 +1,8 @@
 import portfolio from "../assets/projects/portfolio.png";
 import cubesUi from "../assets/projects/cubes-ui.png";
 import smallOne from "../assets/projects/small-one.png";
-import rumi from "../assets/projects/rumi.png";
+import cyrus from "../assets/projects/cyrus.png";
+import pancakesCli from "../assets/projects/pancakes-cli.png";
 export const projects = [
   {
     src: portfolio,
@@ -9,8 +10,7 @@ export const projects = [
     title: "Portfolio",
     description: "You're using it rn",
     buttons: {
-      github:
-"https://github.com/AZCPCF/portfolio",
+      github: "https://github.com/AZCPCF/portfolio",
       live: "https://portfolio-azcpcf-apps.vercel.app",
     },
   },
@@ -20,24 +20,38 @@ export const projects = [
     title: "Cubes UI",
     description: "a little helper",
     buttons: {
-      github: "https://github.com/cubes-ui/cubes-ui",
+      github: "https://github.com/pancakes-ui/cubes-ui",
       live: "https://www.npmjs.com/package/cubes-ui",
     },
   },
   {
-    src: cubesUi,
-    languages: ["js", "ts", "react", "tailwind", "tanstack router"],
-    title: "Cubes Router",
-    description: "folder router using tanstack router",
+    src: pancakesCli,
+    languages: ["js", "node", "ts"],
+    title: "Pancakes Cli",
+    description: "a little cli",
     buttons: {
-      github: "https://github.com/cubes-ui/cubes-router",
-      live: "https://www.npmjs.com/package/cubes-ui",
+      github: "https://github.com/pancakes-ui/pancakes-cli",
+      live: "https://www.npmjs.com/package/pancakes-cli",
     },
-  }, {
-    src:rumi,
-languages:["next","react","js","ts","tailwind"],title:"Rumi Reflection",
-description:"Explore the poetic legacy of Rumi and his timeless spiritual insights.",
-buttons:{github:"https://github.com/AZCPCF/rumi",live:"https://rumi-one.vercel.app"}
+  },
+  {
+    src: cyrus,
+    languages: ["next", "react", "ts", "tailwind", "i18n"],
+    title: "Cyrus Official Website",
+    description: "Cyrus Programming Language Official Website",
+    buttons: {
+      github: "https://github.com/cyrus-lang/Official-Website",
+      live: "https://cyrus-lang-v2.netlify.app/en",
+    },
+  },
+  {
+    src: cyrus,
+    languages: ["node", "js", "ts", "json"],
+    title: "Cyrus Extension",
+    description: "Cyrus Programming Language Official Extension",
+    buttons: {
+      github: "https://github.com/cyrus-lang/Vscode-Extension",
+    },
   },
 ];
 export const smallProjects = [
